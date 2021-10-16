ee754bed6a089294ac96d12e897aecf6b19269ac
Petr Bouchal <pbouchal@gmail.com>
2021-10-16 22:25:07 +0200

    handle readme/index mess

--- R/html_output.R
+++ R/html_output.R
@@ -1,26 +1,38 @@
 t_html <- list(
   tar_file(s_readme_rmd, "README.Rmd"),
-  tar_file(readme_md, ptrr::render_readme(s_readme_rmd)),
-  tar_file(siteconf, "_site.yml"),
-  tar_file(sitefiles, "site"),
-  # https://github.com/jdblischak/workflowr/issues/238#issuecomment-782024069
-  tar_file(s_index_html, command = {
+  tar_file(readme_md, {
     siteconf
     sitefiles
     s_readme_html
-    file.copy("docs/README.html", "docs/index.html")
-    "docs/index.html"}),
+    s_readme_rmd
+    ptrr::render_readme(s_readme_rmd)
+  }),
+  tar_file(siteconf, "_site.yml"),
+  tar_file(sitefiles, "site"),
+  # https://github.com/jdblischak/workflowr/issues/238#issuecomment-782024069
   tar_file(s_readme_html, command = {!! tar_knitr_deps_expr("README.Rmd")
     siteconf
     sitefiles
     rmarkdown::render_site(s_readme_rmd)
     "docs/README.html"}),
+  tar_file(s_index_html, command = {
+    s_readme_html
+    s_readme_rmd
+    # rmarkdown::render_site(s_index_rmd)
+    file.copy("docs/README.html", "docs/index.html", overwrite = TRUE)
+    "docs/index.html"}),
   tar_file(s_pokladnachecks_rmd, "s_pokladnachecks.Rmd"),
   tar_file(s_pokladnachecks_html, command = {!! tar_knitr_deps_expr("s_pokladnachecks.Rmd")
     siteconf
     sitefiles
     rmarkdown::render_site(s_pokladnachecks_rmd)
     "docs/s_pokladnachecks.html"}),
+  tar_file(s_pokladna_rmd, "s_pokladna.Rmd"),
+  tar_file(s_pokladna_html, command = {!! tar_knitr_deps_expr("s_pokladna.Rmd")
+    siteconf
+    sitefiles
+    rmarkdown::render_site(s_pokladna_rmd)
+    "docs/s_pokladna.html"}),
   tar_file(s_doc_rmd, "s_doc.Rmd"),
   tar_file(s_doc_html, command = {!! tar_knitr_deps_expr("s_doc.Rmd")
     siteconf
